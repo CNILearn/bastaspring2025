415565f42e3f124c8926494fe9ad2acebbb28b5b
Christian Nagel <christian@christiannagel.com>
2025-03-02 20:38:48 +0100

    simple lambda parameters

---
+++ csharp/1.3-SimpleLambdaParameters/Program.cs
@@ -0,0 +1,18 @@
+﻿
+// (text, out result ) => int.TryParse(text, out result);
+
+TryParse<int> tryParseInt = (text, out result) => int.TryParse(text, out result);
+
+Console.WriteLine("enter a number");
+string input = Console.ReadLine() ?? string.Empty;
+
+if (tryParseInt(input, out int number))
+{
+    Console.WriteLine($"You entered the number {number}");
+}
+else
+{
+    Console.WriteLine("You did not enter a number");
+}
+
+delegate bool TryParse<T>(string s, out T result);
