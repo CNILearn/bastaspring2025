d4fa2f400997a94616c77c3e8c6ab8aedb46232d
Christian Nagel <christian@christiannagel.com>
2025-03-02 21:08:12 +0100

    method group natural types

---
+++ csharp/1.5-MethodGroupNaturalTypes/Program.cs
@@ -0,0 +1,38 @@
+﻿var c = new C();
+var d = new D();
+d.Del(c.M); // .NET 8, this calls DExt.Del. .NET 9, with the change, it calls D.Del - instance methods first!
+
+public class C
+{
+    public void M()
+    {
+        Console.WriteLine("C.M");
+    }
+}
+
+public static class CExt
+{
+    public static void M(this C c, object o) 
+    {
+        Console.WriteLine("CExt.M");
+    }
+}
+
+public class D
+{
+    public void Del(Delegate d)
+    {
+        Console.WriteLine("D.Del");
+//        d.DynamicInvoke();
+        ((Action)d)();
+    }
+}
+
+public static class DExt
+{
+    public static void Del(this D d, Action<object> action) 
+    {
+        Console.WriteLine("DExt.Del");
+        action(42);
+    }
+}
