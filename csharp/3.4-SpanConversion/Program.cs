6f3201fdfad9b8b9a009f11561702bb11b0997b5
Christian Nagel <christian@christiannagel.com>
2025-03-03 10:40:16 +0100

    span conversion

---
+++ csharp/3.4-SpanConversion/Program.cs
@@ -0,0 +1,30 @@
+﻿int[] data = [1, 2, 3, 4, 5, 6, 7, 8];
+
+ChangeUsingSpan(data);
+ChangeUsingSpan(data.AsSpan()[2..^1]);
+Console.WriteLine();
+
+ReadOnlySpan<int> dataSpan = data;
+
+int[] arr = [1, 2, 3];
+
+Console.WriteLine(
+    arr.StartsWith(1) // CS8773 in C# 13, permitted with C# 14
+    );
+
+void ChangeUsingSpan(Span<int> data)
+{
+    for (int i = 0; i < data.Length; i++)
+    {
+        data[i] += 2;
+    }
+}
+
+public static class MemoryExtensions
+{
+    //public static bool StartsWith<T>(this T[] arr, T value) where T : IEquatable<T> =>
+    //    arr.Length != 0 && EqualityComparer<T>.Default.Equals(arr[0], value);
+
+    public static bool StartsWith<T>(this ReadOnlySpan<T> span, T value) where T : IEquatable<T> => 
+        span.Length != 0 && EqualityComparer<T>.Default.Equals(span[0], value);
+}
