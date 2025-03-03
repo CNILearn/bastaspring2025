43d43d4a6119d73347aacaef2f41cb0ef09602a6
Christian Nagel <christian@christiannagel.com>
2025-03-03 14:35:13 +0100

    config and logging

---
+++ DI/05-ConfigLogging-GreetingSample/Program.cs
@@ -0,0 +1,14 @@
+﻿using GreetingsSample;
+
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+var builder = Host.CreateApplicationBuilder();
+builder.Services.AddTransient<IHelloService, HelloService>();
+builder.Services.AddTransient<HomeController>();
+builder.Services.Configure<HelloServiceOptions>(builder.Configuration.GetSection("HelloService"));
+using var host = builder.Build();
+
+HomeController controller = host.Services.GetRequiredService<HomeController>();
+string result = controller.Index("Angela");
+Console.WriteLine(result);
