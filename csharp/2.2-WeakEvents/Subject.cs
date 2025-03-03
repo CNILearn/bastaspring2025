30dec31ee307cc632a4d29cbd7f6ebac8fc8db16
Christian Nagel <christian@christiannagel.com>
2025-03-03 09:52:39 +0100

    weak events

---
+++ csharp/2.2-WeakEvents/Subject.cs
@@ -0,0 +1,27 @@
+﻿namespace WeakEvents;
+
+public partial class Subject(int id)
+{
+    public int Id { get; } = id;
+
+    // this is in progress - C# 14
+    // public partial event EventHandler<SubjectEventArgs> SomeEvent;
+
+    private readonly WeakEvent<SubjectEventArgs> _weakEvent = new();
+    public event EventHandler<SubjectEventArgs> SomeEvent
+    {
+        add
+        {
+            _weakEvent.AddHandler(value);
+        }
+        remove
+        {
+            _weakEvent.RemoveHandler(value);
+        }
+    }
+
+    public void RaiseEvent()
+    {
+        _weakEvent.RaiseEvent(this, new SubjectEventArgs(Id));
+    }
+}
