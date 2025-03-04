3a239f55d341223be0571c2370036ebcf1f3bb97
Christian Nagel <christian@christiannagel.com>
2025-03-04 18:01:40 +0100

    lifetime

---
+++ DI/1.6-Lifetime/SingletonService.cs
@@ -0,0 +1,25 @@
+﻿namespace DisposableServices;
+
+internal class SingletonService : IDisposable
+{
+    private int _number = 0;    
+    public SingletonService(NumberService numberService)
+    {
+        _number = numberService.GetNumber(nameof(SingletonService));
+        Console.WriteLine($"{nameof(SingletonService)} {_number} created");
+    }
+
+    private bool _isDisposed = false;
+    public void Foo()
+    {
+        ObjectDisposedException.ThrowIf(_isDisposed, this);
+
+        Console.WriteLine("Foo called");
+    }
+
+    public void Dispose()
+    {
+        Console.WriteLine($"{nameof(SingletonService)} disposed");
+        _isDisposed = true;
+    }
+}
