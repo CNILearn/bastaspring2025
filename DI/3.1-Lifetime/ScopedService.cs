28174421417cfa692074fa48a8296dec996e379d
Christian Nagel <christian@christiannagel.com>
2025-03-04 23:19:14 +0100

    lifetime

---
+++ DI/3.1-Lifetime/ScopedService.cs
@@ -0,0 +1,24 @@
+﻿namespace DisposableServices;
+
+internal class ScopedService : IDisposable
+{
+    private int _number = 0;
+    public ScopedService(NumberService numberService)
+    {
+        _number = numberService.GetNumber(nameof(ScopedService));
+        Console.WriteLine($"{nameof(ScopedService)} {_number} created");
+    }
+
+    private bool _isDisposed = false;
+    public void Foo()
+    {
+        ObjectDisposedException.ThrowIf(_isDisposed, this);
+        Console.WriteLine("Foo called");
+    }
+
+    public void Dispose()
+    {
+        Console.WriteLine($"{nameof(ScopedService)} disposed");
+        _isDisposed = true;
+    }
+}
