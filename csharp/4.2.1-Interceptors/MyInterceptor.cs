f0ce3a7150384e9ce6bf4e03aa357c5d23be3a16
Christian Nagel <christian@christiannagel.com>
2025-03-04 14:05:25 +0100

    fix interceptor path

--- csharp/4.2.1-Interceptors/MyInterceptor.cs
+++ csharp/4.2.1-Interceptors/MyInterceptor.cs
@@ -3,13 +3,13 @@
 namespace Interceptors;
 public static class MyInterceptor
 {
-    [InterceptsLocation(@"D:\cnilearn\bastaspring2025\csharp\4.2.2-Interceptors\Runner.cs", 7, 9)]
+    [InterceptsLocation(@"D:\cnilearn\bastaspring2025\csharp\4.2.1-Interceptors\Runner.cs", 7, 9)]
     public static void InterceptDotheMagic(int x)
     {
         Console.WriteLine($"Intercepting DotheMagic with {x}");
     }
 
-    [InterceptsLocation(@"D:\cnilearn\bastaspring2025\csharp\4.2.2-Interceptors\Runner.cs", 9, 9)]
+    [InterceptsLocation(@"D:\cnilearn\bastaspring2025\csharp\4.2.1-Interceptors\Runner.cs", 9, 9)]
     public static void FooMagic(this Runner r)
     {
         Console.WriteLine($"Intercepting Foo");
--- csharp/4.2.1-Interceptors/Runner.cs
+++ csharp/4.2.1-Interceptors/Runner.cs
@@ -5,7 +5,7 @@ public class Runner
     public void Bar()
     {
         DoTheMagic(42); // this method will be replaced
-        DoTheMagic(3); // this method will do the Foo 
+        DoTheMagic(3); // this method will do the Dothemagic 
         Foo();  
         Foo();
     }
