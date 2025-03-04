53d1f024c9c2678dc2e48e01016343a91ef1128b
Christian Nagel <christian@christiannagel.com>
2025-03-04 07:48:42 +0100

    folder reorg

---
+++ csharp/4.2.1-Interceptors/InterceptsLocation.cs
@@ -0,0 +1,7 @@
+﻿namespace System.Runtime.CompilerServices;
+
+// TODO: switch to https://github.com/dotnet/roslyn/issues/72133 with a source generator
+[AttributeUsage(AttributeTargets.Method, AllowMultiple = true)]
+sealed class InterceptsLocationAttribute(string filePath, int line, int column) : Attribute
+{
+}
